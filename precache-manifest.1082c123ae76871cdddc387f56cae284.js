b16e5d00bdde86c626fc6bc91545341ccd1ce4ac
nevstafev <evstafev.n.e@gmail.com>
2021-04-23 22:51:22 +0300

    Updates

---
+++ precache-manifest.1082c123ae76871cdddc387f56cae284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498ff61a90077de2f6861694c1ef2301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac8f62d68f387444640d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e74d485ed086a9436b3d",
+    "url": "/static/js/2.5d0b362b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d0b362b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8f62d68f387444640d",
+    "url": "/static/js/main.0ea36d61.chunk.js"
+  },
+  {
+    "revision": "f5db916bff56257f8379",
+    "url": "/static/js/runtime-main.2a28dbf8.js"
+  }
+]);
